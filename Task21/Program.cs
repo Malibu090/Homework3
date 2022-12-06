769bd281d525913d7d8c13061aa5abad5434f53f
Malibu <malibu090@mail.ru>
2022-12-06 18:06:16 +0300

    1

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -14,7 +14,7 @@ int DataEntryXY(string str)
 double DistancePoints(int x1, int y1, int z1, int x2, int y2, int z2)
 {
     double count = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) +Math.Pow((z2 - z1), 2));
-    //    double result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    //    double count = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
     return count;
 }
 
