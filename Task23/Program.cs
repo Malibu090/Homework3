d80aeec1ef2bef0b04fb46b3d33c0a32e89e9318
Malibu <malibu090@mail.ru>
2022-12-06 18:03:23 +0300

    Таблица кубов

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+void TablePOW3(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    int count = 1;
+    while (count <= number)
+    {
+        int pow = count * count * count;
+        Console.WriteLine(pow + " ");
+        count++;
+    }
+    Console.WriteLine();
+}
+
+TablePOW3("Введите число N: ");
